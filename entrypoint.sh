55e775a5493ec37607ec88abfe5c70ac38b97131
Bryan Tomlin <bryan@tomlin.email>
2025-08-07 20:03:00 +0000

    tweak enrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,3 @@ wgo -file=.go -file=.templ -file="^assetsSrc/" -xfile=_templ.go -exit go tool te
   :: ./bin/tailwindcss -i ./input.css -o ./web/assetsSrc/css/styles.css \
   :: go run ./cmd/fingerprint/ \
   :: go run ./cmd/server/
-  :: &
